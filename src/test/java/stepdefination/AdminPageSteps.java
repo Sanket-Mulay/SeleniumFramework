5edc091583170404be3593aad8988af78feb6793
Sanket-Mulay <sanketmulay2024@gmail.com>
2026-02-10 14:52:38 +0530

    username changes

--- src/test/java/stepdefination/AdminPageSteps.java
+++ src/test/java/stepdefination/AdminPageSteps.java
@@ -80,7 +80,7 @@ public void enter_valid_user_role() {
 
 @When("Search User Name")
 public void search_user_name() {
-	adminPage.selectUserFromAutoSuggestion("mandaTeja akhil user");
+	adminPage.selectUserFromAutoSuggestion("perf user test");
 }
 
 @When("Enter User Status")
